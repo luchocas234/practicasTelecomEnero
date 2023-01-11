a191dafc8914495a6d78239b6a8b5ff8e077e031
Luciano Castro <lfcastro2911@gmail.com>
2023-01-11 15:45:13 -0300

    Update Contents.swift

--- practicasTelecomEnero/Playgrounds/Strings.playground/Contents.swift
+++ practicasTelecomEnero/Playgrounds/Strings.playground/Contents.swift
@@ -1,5 +1,32 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//Comparacion
 
-print(greeting)
+let name1 = "Pedro"
+let name2 = "Juan"
+let name3 = "Pedro"
+//false
+name1 == name2
+//true
+name1 == name3
+
+//Contiene en el array
+
+let array1 = "Esto es un array de Pedro"
+let array2 = ""
+array1.contains(name1)
+array1.contains("array")
+
+array1.contains(name2)
+
+//Vacia
+
+array2 == ""
+array2.isEmpty
+
+//Recorrido
+
+for content in array1 {
+    print(content)
+}
+
