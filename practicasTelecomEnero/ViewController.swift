6f91669207d25ad429de12ca59716fb243b850a7
Luciano Castro <lfcastro2911@gmail.com>
2023-01-16 21:24:13 -0300

    Buttons Practice

--- practicasTelecomEnero/ViewController.swift
+++ practicasTelecomEnero/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //Mybutton
+        myButton.setTitle("ChangeColor", for: .normal)
+        myButton.backgroundColor = .blue
+        myButton.setTitleColor(.white, for: .normal)
+        
+        
     }
 
-
+    @IBAction func myButtonAction(_ sender: UIButton) {
+        if(myButton.backgroundColor == .blue){
+            myButton.backgroundColor = .green
+        }else{
+            myButton.backgroundColor = .blue 
+        }
+    }
+    
 }
 
